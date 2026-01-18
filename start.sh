db1a8c1651a36d09c23d44487964c0427861e49e
Mike Gee <mpgee@usc.edu>
2026-01-17 18:53:41 -0800

    Updated README.md so it contains the instructions I used to get the website to run locally

--- start.sh
+++ start.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-
-# Script to start Jekyll development server in WSL
-# This script will:
-# 1. Start WSL
-# 2. Navigate to the website project directory
-# 3. Start Jekyll server with livereload and custom build directory
-
-echo "Starting Jekyll development server in WSL..."
-
-# Start WSL and execute the commands
-wsl bash -c "
-cd /mnt/c/Users/Mike/vscode/mpg05883.github.io
-
-# Set environment variable and start Jekyll
-LISTEN_POLLING=1 bundle exec jekyll serve \
-  --livereload \
-  --host localhost --port 4000 \
-  --destination ~/jekyll_build
-"
-
-echo "WSL session ended."
+bundle exec jekyll serve -l -H localhost --incremental
